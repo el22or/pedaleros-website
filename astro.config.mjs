beabfee2c9dee2897a63b555967200465c98dbe1
Dragan Eror <draganeror@gmail.com>
2026-02-19 23:28:01 +0100

    configure github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  site: 'https://el22or.github.io', // or custom domain
+  base: 'pedaleros-website',        // omit or '/' if using user site repo
+});
